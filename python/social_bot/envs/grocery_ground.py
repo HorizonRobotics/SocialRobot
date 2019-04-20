e7d46c76305980c1084829773d54f9e3758127d0
Jiangtao <sdu1028@gmail.com>
2019-04-20 18:52:03 +0800

    Add teacher for the environment

--- python/social_bot/envs/grocery_ground.py
+++ python/social_bot/envs/grocery_ground.py
@@ -5,6 +5,7 @@ A simple enviroment for an agent play on a groceryground
 import os
 import logging
 import numpy as np
+import math
 import random
 import PIL
 
@@ -15,31 +16,58 @@ from collections import OrderedDict
 import social_bot
 from social_bot import teacher
 from social_bot.teacher import TeacherAction
+from social_bot.envs import simple_navigation
+from social_bot.envs.simple_navigation import GoalTask
+from social_bot.envs.simple_navigation import DiscreteSequence
 import social_bot.pygazebo as gazebo
 
 logger = logging.getLogger(__name__)
 
 
-def rgb2gray(rgb):
-    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
-
-
-class DiscreteSequence(gym.Space):
+class GroceryGroundGoalTask(GoalTask):
     """
-    gym.Space object for language sequence
+    For this task, the agent will receive reward 1 when it is close enough to the goal.
+    If it is moving away from the goal too much or still not close to the goal after max_steps,
+    it will get reward -1.
     """
 
-    def __init__(self, vocab_size, max_length):
+    def __init__(self,
+                 max_steps=500,
+                 goal_name="bookshelf",
+                 success_distance_thresh=0.5,
+                 fail_distance_thresh=3.0,
+                 goal_random_range=20.0):
         """
         Args:
-            vocab_size (int): number of different tokens
-            max_length (int): maximal length of the sequence
+            max_steps (int): episode will end if not reaching gaol in so many steps
+            goal_name (string): name of the goal in the world
+            success_distance_thresh (float): the goal is reached if it's within this distance to the agent
+            fail_distance_thresh (float): if the agent moves away from the goal more than this distance,
+                it's considered a failure and is givne reward -1
+            goal_random_range (float): the goal's random position range
+            
         """
-        super()
-        self._vocab_size = vocab_size
-        self._max_length = max_length
-        self.dtype = np.int32
-        self.shape = (max_length)
+        self._goal_random_range = goal_random_range
+        super(GroceryGroundGoalTask, self).__init__(
+            max_steps=max_steps,
+            goal_name=goal_name,
+            success_distance_thresh=success_distance_thresh,
+            fail_distance_thresh=fail_distance_thresh)
+
+    def _move_goal(self, goal, agent_loc):
+        while True:
+            loc = (random.random() * self._goal_random_range -
+                   self._goal_random_range / 2,
+                   random.random() * self._goal_random_range -
+                   self._goal_random_range / 2, 0)
+            self._initial_dist = np.linalg.norm(loc - agent_loc)
+            if self._initial_dist > 0.5:
+                break
+        goal.set_pose((loc, (0, 0, 0)))
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
 
 class GroceryGround(gym.Env):
@@ -89,12 +117,17 @@ class GroceryGround(gym.Env):
             'plastic_cup_2', 'plastic_cup_3'
         ]
         self._goal_name = 'bookshelf'
+        self._object_name = 'trash_can'
+
+        self._teacher = teacher.Teacher(False)
+        task_group = teacher.TaskGroup()
+        task_group.add_task(GroceryGroundGoalTask())
+        self._teacher.add_task_group(task_group)
 
         self._max_steps = max_steps
         self._with_language = with_language
         self._use_image_obs = use_image_obs
         self._resized_image_size = (84, 84)
-        self.object_name = 'caffe_table'
 
         obs = self.reset()
         if self._use_image_obs:
@@ -103,33 +136,39 @@ class GroceryGround(gym.Env):
         else:
             obs_data_space = gym.spaces.Box(
                 low=-50, high=50, shape=obs.shape, dtype=np.float32)
+        control_space = gym.spaces.Box(
+            low=-20, high=20, shape=[len(self._pr2_joints)], dtype=np.float32)
 
         if self._with_language:
             self.observation_space = gym.spaces.Dict(
-                data=obs_data_space, sentence=DiscreteSequence(64, 21))
+                data=obs_data_space, sentence=DiscreteSequence(128, 24))
+            self._action_space = gym.spaces.Dict(
+                control=control_space, sentence=DiscreteSequence(128, 24))
         else:
             self.observation_space = obs_data_space
-
-        self.action_space = gym.spaces.Box(
-            low=-20, high=20, shape=[len(self._pr2_joints)], dtype=np.float32)
+            self.action_space = control_space
 
     def reset(self):
         """
         Reset the environment
         Args: None
+        Returns: Observaion of the first step
         """
         self._world.reset()
         self._world.step(5)
         self._collision_cnt = 0
         self._cum_reward = 0.0
         self._steps_in_this_episode = 0
-        self._goal = self._world.get_agent(self._goal_name)
-        if self._with_language:
-            obj_id = random.randint(0, len(self._objects_names) - 1)
-            self.object_name = self._objects_names[obj_id]
-            logger.debug("object_name:" + self.object_name)
         self._prev_dist = self._get_object_distance()
-        return self._get_observation()
+        self._goal = self._world.get_agent(self._goal_name)
+        self._teacher.reset(self._agent, self._world)
+        teacher_action = self._teacher.teach("")
+        if self._with_language:
+            obs_data = self._get_observation()
+            obs = OrderedDict(data=obs_data, sentence=teacher_action)
+        else:
+            obs = self._get_observation()
+        return obs
 
     def _get_observation(self):
         if self._use_image_obs:
@@ -152,7 +191,7 @@ class GroceryGround(gym.Env):
         return obs_data
 
     def _get_object_distance(self):
-        object_loc, _ = self._world.get_agent(self.object_name).get_pose()
+        object_loc, _ = self._world.get_agent(self._object_name).get_pose()
         agent_loc, _ = self._agent.get_pose()
         dist = np.linalg.norm(np.array(object_loc) - np.array(agent_loc))
         return dist
@@ -175,12 +214,19 @@ class GroceryGround(gym.Env):
             If with_language, it is a dictionary with key 'data' and 'sentence'
             If not with_language, it is a numpy.array or image for observation
         """
-        controls = dict(zip(self._pr2_joints, action))
+        if self._with_language:
+            sentence = action.get('sentence', None)
+            controls = action['control']
+        else:
+            sentence = ''
+            controls = action
+        controls = dict(zip(self._pr2_joints, controls))
+        teacher_action = self._teacher.teach(sentence)
         self._agent.take_action(controls)
         self._world.step(5)
         if self._with_language:
             obs_data = self._get_observation()
-            obs = OrderedDict(data=obs_data, sentence=self.object_name)
+            obs = OrderedDict(data=obs_data, sentence=teacher_action.sentence)
         else:
             obs = self._get_observation()
         reward = self._compute_reward()
@@ -189,10 +235,16 @@ class GroceryGround(gym.Env):
         self._cum_reward += reward
         if done:
             logger.debug("episode ends at cum reward:" + str(self._cum_reward))
-        return obs, reward, done, {}
+        if self._with_language:
+            return obs, teacher_action.reward, teacher_action.done, {}
+        else:
+            return obs, reward, done, {}
 
 
 def main():
+    """
+    Simple testing of this enviroenment.
+    """
     env = GroceryGround(port=11345)
     while True:
         actions = 20 * np.random.randn(env.action_space.shape[0])
