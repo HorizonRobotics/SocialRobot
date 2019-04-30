16b79f93d31475032cfa7cab19f678ab02b96062
Jiangtao <sdu1028@gmail.com>
2019-04-30 17:40:39 +0800

    Remove dependency to example of tfagent to avoid importing mujoco (no lic)

--- examples/train_grocery_ground.py
+++ examples/train_grocery_ground.py
@@ -1,5 +1,13 @@
 # Copyright (c) 2019 Horizon Robotics. All Rights Reserved.
 #
+# A simple test for SocialBot-GroceryGround env use TF-Agents
+# The original file is from the TD3 example of TF-Agents:
+#
+#     https://github.com/tensorflow/agents
+#
+# Some parameters are modifed suite the environment.
+#
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -11,32 +19,275 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Train and Eval GroceryGround with TD3.
 
+To run:
+
+```bash
+examples/train_grocery_ground -- \
+  --root_dir=$HOME/tmp/SocialBot-GroceryGround-v0/ \
+  --num_iterations=2000000 \
+  --alsologtostderr
+```
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import time
+
+from absl import app
+from absl import flags
 from absl import logging
+
 import tensorflow as tf
+
+from tf_agents.agents.ddpg import actor_network
+from tf_agents.agents.ddpg import critic_network
+from tf_agents.agents.td3 import td3_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import tf_py_environment
+from tf_agents.metrics import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.utils import common
+import gin.tf
+
 import social_bot
 from alf.environments import suite_socialbot
-from tf_agents.agents.ddpg.examples.v2.train_eval import train_eval
+
+flags.DEFINE_string('root_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
+                    'Root directory for writing logs/summaries/checkpoints.')
+flags.DEFINE_integer('num_iterations', 100000,
+                     'Total number train/eval iterations to perform.')
+flags.DEFINE_multi_string('gin_file', None, 'Paths to the gin-config files.')
+flags.DEFINE_multi_string('gin_param', None, 'Gin binding parameters.')
+
+FLAGS = flags.FLAGS
 
 
-def train_with_ddpg():
+@gin.configurable
+def train_eval(
+        root_dir,
+        env_name='SocialBot-GroceryGround-v0',
+        num_iterations=2000000,
+        actor_fc_layers=(256, 128, 128),
+        critic_obs_fc_layers=(400, ),
+        critic_action_fc_layers=None,
+        critic_joint_fc_layers=(256, 128),
+        # Params for collect
+        initial_collect_steps=1000,
+        collect_steps_per_iteration=1,
+        replay_buffer_capacity=100000,
+        ou_stddev=0.2,
+        ou_damping=0.15,
+        # Params for target update
+        target_update_tau=0.05,
+        target_update_period=5,
+        # Params for train
+        train_steps_per_iteration=1,
+        batch_size=64,
+        actor_learning_rate=1e-4,
+        critic_learning_rate=1e-3,
+        dqda_clipping=None,
+        td_errors_loss_fn=tf.compat.v1.losses.huber_loss,
+        gamma=0.995,
+        reward_scale_factor=1.0,
+        gradient_clipping=None,
+        use_tf_functions=True,
+        # Params for eval
+        num_eval_episodes=10,
+        eval_interval=10000,
+        # Params for checkpoints, summaries, and logging
+        log_interval=1000,
+        summary_interval=1000,
+        summaries_flush_secs=10,
+        debug_summaries=False,
+        summarize_grads_and_vars=False,
+        eval_metrics_callback=None):
+    """A simple train and eval for TD3."""
+    root_dir = os.path.expanduser(root_dir)
+    train_dir = os.path.join(root_dir, 'train')
+    eval_dir = os.path.join(root_dir, 'eval')
+
+    train_summary_writer = tf.compat.v2.summary.create_file_writer(
+        train_dir, flush_millis=summaries_flush_secs * 1000)
+    train_summary_writer.set_as_default()
+
+    eval_summary_writer = tf.compat.v2.summary.create_file_writer(
+        eval_dir, flush_millis=summaries_flush_secs * 1000)
+    eval_metrics = [
+        tf_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
+        tf_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes)
+    ]
+
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    with tf.compat.v2.summary.record_if(
+            lambda: tf.math.equal(global_step % summary_interval, 0)):
+        tf_env = tf_py_environment.TFPyEnvironment(
+            suite_socialbot.load(env_name))
+        eval_tf_env = tf_py_environment.TFPyEnvironment(
+            suite_socialbot.load(env_name))
+
+        actor_net = actor_network.ActorNetwork(
+            tf_env.time_step_spec().observation,
+            tf_env.action_spec(),
+            fc_layer_params=actor_fc_layers,
+        )
+
+        critic_net_input_specs = (tf_env.time_step_spec().observation,
+                                  tf_env.action_spec())
+
+        critic_net = critic_network.CriticNetwork(
+            critic_net_input_specs,
+            observation_fc_layer_params=critic_obs_fc_layers,
+            action_fc_layer_params=critic_action_fc_layers,
+            joint_fc_layer_params=critic_joint_fc_layers,
+        )
+
+        tf_agent = td3_agent.Td3Agent(
+            tf_env.time_step_spec(),
+            tf_env.action_spec(),
+            actor_network=actor_net,
+            critic_network=critic_net,
+            actor_optimizer=tf.compat.v1.train.AdamOptimizer(
+                learning_rate=actor_learning_rate),
+            critic_optimizer=tf.compat.v1.train.AdamOptimizer(
+                learning_rate=critic_learning_rate),
+            ou_stddev=ou_stddev,
+            ou_damping=ou_damping,
+            target_update_tau=target_update_tau,
+            target_update_period=target_update_period,
+            dqda_clipping=dqda_clipping,
+            td_errors_loss_fn=td_errors_loss_fn,
+            gamma=gamma,
+            reward_scale_factor=reward_scale_factor,
+            gradient_clipping=gradient_clipping,
+            debug_summaries=debug_summaries,
+            summarize_grads_and_vars=summarize_grads_and_vars,
+            train_step_counter=global_step,
+        )
+        tf_agent.initialize()
+
+        train_metrics = [
+            tf_metrics.NumberOfEpisodes(),
+            tf_metrics.EnvironmentSteps(),
+            tf_metrics.AverageReturnMetric(),
+            tf_metrics.AverageEpisodeLengthMetric(),
+        ]
+
+        eval_policy = tf_agent.policy
+        collect_policy = tf_agent.collect_policy
+
+        replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+            tf_agent.collect_data_spec,
+            batch_size=tf_env.batch_size,
+            max_length=replay_buffer_capacity)
+
+        initial_collect_driver = dynamic_step_driver.DynamicStepDriver(
+            tf_env,
+            collect_policy,
+            observers=[replay_buffer.add_batch],
+            num_steps=initial_collect_steps)
+
+        collect_driver = dynamic_step_driver.DynamicStepDriver(
+            tf_env,
+            collect_policy,
+            observers=[replay_buffer.add_batch] + train_metrics,
+            num_steps=collect_steps_per_iteration)
+
+        if use_tf_functions:
+            initial_collect_driver.run = common.function(
+                initial_collect_driver.run)
+            collect_driver.run = common.function(collect_driver.run)
+            tf_agent.train = common.function(tf_agent.train)
+
+        # Collect initial replay data.
+        logging.info(
+            'Initializing replay buffer by collecting experience for %d steps with '
+            'a random policy.', initial_collect_steps)
+        initial_collect_driver.run()
+
+        results = metric_utils.eager_compute(
+            eval_metrics,
+            eval_tf_env,
+            eval_policy,
+            num_episodes=num_eval_episodes,
+            train_step=global_step,
+            summary_writer=eval_summary_writer,
+            summary_prefix='Metrics',
+        )
+        if eval_metrics_callback is not None:
+            eval_metrics_callback(results, global_step.numpy())
+        metric_utils.log_metrics(eval_metrics)
+
+        time_step = None
+        policy_state = collect_policy.get_initial_state(tf_env.batch_size)
+
+        timed_at_step = global_step.numpy()
+        time_acc = 0
+
+        # Dataset generates trajectories with shape [Bx2x...]
+        dataset = replay_buffer.as_dataset(
+            num_parallel_calls=3, sample_batch_size=batch_size,
+            num_steps=2).prefetch(3)
+        iterator = iter(dataset)
+
+        for _ in range(num_iterations):
+            start_time = time.time()
+            time_step, policy_state = collect_driver.run(
+                time_step=time_step,
+                policy_state=policy_state,
+            )
+            for _ in range(train_steps_per_iteration):
+                experience, _ = next(iterator)
+                train_loss = tf_agent.train(experience)
+            time_acc += time.time() - start_time
+
+            if global_step.numpy() % log_interval == 0:
+                logging.info('step = %d, loss = %f', global_step.numpy(),
+                             train_loss.loss)
+                steps_per_sec = (
+                    global_step.numpy() - timed_at_step) / time_acc
+                logging.info('%.3f steps/sec', steps_per_sec)
+                tf.compat.v2.summary.scalar(
+                    name='global_steps_per_sec',
+                    data=steps_per_sec,
+                    step=global_step)
+                timed_at_step = global_step.numpy()
+                time_acc = 0
+
+            for train_metric in train_metrics:
+                train_metric.tf_summaries(
+                    train_step=global_step, step_metrics=train_metrics[:2])
+
+            if global_step.numpy() % eval_interval == 0:
+                results = metric_utils.eager_compute(
+                    eval_metrics,
+                    eval_tf_env,
+                    eval_policy,
+                    num_episodes=num_eval_episodes,
+                    train_step=global_step,
+                    summary_writer=eval_summary_writer,
+                    summary_prefix='Metrics',
+                )
+                if eval_metrics_callback is not None:
+                    eval_metrics_callback(results, global_step.numpy())
+                metric_utils.log_metrics(eval_metrics)
+
+        return train_loss
+
+
+def main(_):
     tf.compat.v1.enable_v2_behavior()
     logging.set_verbosity(logging.INFO)
-    train_eval(
-        root_dir='~/tmp/ddpg/SocialBot-GroceryGround',
-        env_name='SocialBot-GroceryGround-v0',
-        env_load_fn=suite_socialbot.load,
-        num_iterations=20000000,
-        actor_fc_layers=(192, 192, 128),
-        critic_obs_fc_layers=(192, ),
-        critic_action_fc_layers=(128, ),
-        critic_joint_fc_layers=(192, 128),
-        # Params for collect
-        initial_collect_steps=1200,
-        collect_steps_per_iteration=1,
-        num_parallel_environments=1,
-        replay_buffer_capacity=100000)
+    gin.parse_config_files_and_bindings(FLAGS.gin_file, FLAGS.gin_param)
+    train_eval(FLAGS.root_dir, num_iterations=FLAGS.num_iterations)
 
 
 if __name__ == '__main__':
-    train_with_ddpg()
+    flags.mark_flag_as_required('root_dir')
+    app.run(main)
