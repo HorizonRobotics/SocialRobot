60f9b6f12843d7fb1b887b8b9e3b3f65a30613de
Haonan Yu <haonanu@gmail.com>
2019-06-03 16:42:04 -0700

    gin-config version to be consistent with tf_agents setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ for f in glob.glob("build/pygazebo/pygazebo*.so"):
 setup(
     name='social_bot',
     version='0.0.1',
-    install_requires=['gin-config', 'gym', 'numpy', 'matplotlib', 'psutil'],
+    install_requires=['gin-config == 0.1.3', 'gym', 'numpy', 'matplotlib', 'psutil'],
     package_dir={'': 'python'},
     packages=find_packages('python'),
     package_data={'social_bot': ['models', 'worlds', 'pygazebo*.so']},
