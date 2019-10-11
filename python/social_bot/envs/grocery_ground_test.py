9e979a1cef71089a0b46fdd097102d28b59c3087
Jiangtao <sdu1028@gmail.com>
2019-10-11 11:09:47 +0800

    using pre-commit license info

--- python/social_bot/envs/grocery_ground_test.py
+++ python/social_bot/envs/grocery_ground_test.py
@@ -1,4 +1,16 @@
 # Copyright (c) 2019 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import unittest
 import random
@@ -19,7 +31,7 @@ class TestGroceryGround(unittest.TestCase):
             for task_name in tasks:
                 for use_image_obs in [True, False]:
                     if agent_type == 'icub' and use_image_obs:
-                        continue # icub does not have camera
+                        continue
                     logging.info("Testing Case: Agent " + agent_type +
                                  ", Task " + task_name + ", UseImage: " +
                                  str(use_image_obs))
