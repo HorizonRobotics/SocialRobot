370c7a4cdcc76a81d130b9f2cc1f3752c023f60b
Jiangtao <sdu1028@gmail.com>
2019-11-13 18:07:44 +0800

    minior fix

--- python/social_bot/envs/play_ground.py
+++ python/social_bot/envs/play_ground.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-A simple enviroment for an agent play on the ground
+A simple enviroment for an agent playing on the ground
 """
 import os
 import time
