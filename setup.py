7460efd7d10feeab0ae744a3d1fe0312506585b9
Le Horizon <le.zhao+git@horizon.ai>
2022-04-29 11:30:19 -0700

    Revert "update alf image"
    This reverts commit cc5c764ede2ef8769506a50105ef01c46875b67b.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'pytest',
         'PyUserInput',
         'python-xlib',
+        'alf@git+https://github.com/HorizonRobotics/alf.git',
     ],
     package_dir={'': 'python'},
     packages=find_packages('python'),
