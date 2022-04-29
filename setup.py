38a38c9915b71421b0ba728b61eb9e2e086f7835
Le Horizon <le.zhao+git@horizon.ai>
2022-04-28 22:32:00 -0700

    test 2 fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='social_bot',
     version='0.0.1',
     install_requires=[
-        'alf@git+https://github.com/HorizonRobotics/alf.git@0.0.6#egg=alf',
+        'alf@git+https://github.com/HorizonRobotics/alf.git@v0.0.6#egg=alf',
         'gin-config@git+https://github.com/HorizonRobotics/gin-config.git',
         'gym == 0.15.4',
         'numpy >= 1.13.3',
